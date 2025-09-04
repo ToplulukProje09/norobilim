031af3194a103cbb3dfcbea0957d021e10d7e375
EfeTekeli <efetekeli09@outlook.com>
2025-09-04 20:14:31 +0300

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,17 +6,9 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const DATABASE_URL =
-  "mongodb+srv://toplulukproje_db_user:yDDNlT7CnNdM3i1N@cluster0.fjsjezr.mongodb.net/norobilimadu?retryWrites=true&w=majority&appName=Cluster0";
-
 export const prisma =
   globalThis.prisma ||
   new PrismaClient({
-    datasources: {
-      db: {
-        url: DATABASE_URL,
-      },
-    },
     log:
       process.env.NODE_ENV === "development"
         ? ["query", "error", "warn"]
