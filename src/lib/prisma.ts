989e0690747c8516336042d086987dba23015831
EfeTekeli <efetekeli09@outlook.com>
2025-09-04 19:24:02 +0300

    deneme

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,22 @@
 // lib/prisma.ts
+
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  // Hot-reload sırasında duplicate prisma client oluşmasını engeller
   var prisma: PrismaClient | undefined;
 }
+
+const DATABASE_URL =
+  "mongodb+srv://toplulukproje_db_user:yDDNlT7CnNdM3i1N@cluster0.fjsjezr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; // <-- ENV yerine buraya yapıştır
 
 export const prisma =
   globalThis.prisma ||
   new PrismaClient({
+    datasources: {
+      db: {
+        url: DATABASE_URL,
+      },
+    },
     log:
       process.env.NODE_ENV === "development"
         ? ["query", "error", "warn"]
